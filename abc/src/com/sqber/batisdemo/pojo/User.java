85663aff8bad8caf36f70b9418a50a879d377064
sqb <shenqiangbin@163.com>
2018-04-13 16:51:52 +0800

    修改

--- abc/src/com/sqber/batisdemo/pojo/User.java
+++ abc/src/com/sqber/batisdemo/pojo/User.java
@@ -1,5 +1,16 @@
 package com.sqber.batisdemo.pojo;
 
 public class User {
-
+	private int userId;
+	private String userCode;
+	private String userName;
+	private int status;
+	
+	@Override
+	public String toString() {
+		return "userId:"+userId
+			+" userCode:"+userCode
+			+" userName:"+userName
+			+" status:"+status;
+	}
 }
